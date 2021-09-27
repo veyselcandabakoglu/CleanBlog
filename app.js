83ae0437c32ad31c2e6a20e0d428b67d7cfc21b8
Veysel CANDABAKOĞLU <vcandabakoglu@ayk.gov.tr>
2021-09-27 16:30:57 +0300

    Some files updated, some files created.

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+const Post = require('./module/Post');
 
 const app = express();
 
-// TEMPLATE ENGİNE
+// CONNECT DB
+
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 // MIDDLEWARE
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // ROUTE
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index', {
+    posts,
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -22,11 +36,12 @@ app.get('/add_post', (req, res) => {
   res.render('add_post');
 });
 
-app.get('/add_post', (req, res) => {
-  res.render('add_post');
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
 });
 
-const port = 3000;
+const port = 3001;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı...`);
 });
